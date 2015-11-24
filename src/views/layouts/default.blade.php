a58170ee8c7cc2e34e5d090b715d63cf8673f8b7
Victor Aguilar <victor.aguilar@ciencias.unam.mx>
2015-11-23 22:46:42 -0600

    Added links and scripts to the default layout.

--- src/views/layouts/default.blade.php
+++ src/views/layouts/default.blade.php
@@ -2,9 +2,9 @@
 <html>
 <head>
     <title>@yield('title', 'Filmoteca')</title>
+    <link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/themes/smoothness/jquery-ui.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
-    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-    <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="{{ asset('/packages/filmoteca/static-pages/stylesheets/menu-creator.css') }}">
 </head>
 <body>
     <div class="container-fluid">
@@ -13,5 +13,10 @@
 
         @yield('content')
     </div>
+
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+    <script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/jquery-ui.min.js"></script>
+    <script src="{{ asset('/packages/filmoteca/static-pages/scripts/menu-creator.js') }}"></script>
 </body>
 </html>
